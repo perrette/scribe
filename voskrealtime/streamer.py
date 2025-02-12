4ec8bd981687d7bfd8acdc434a95a5369cf76f79
perrette <mahe.perrette@pik-potsdam.de>
2025-02-12 17:55:49 +0100

    proper python packaging

--- voskrealtime/streamer.py
+++ voskrealtime/streamer.py
@@ -22,84 +22,85 @@ language_config_default = {
 }
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--language", nargs="+", choices=list(language_config_default),
-                    default=["en"])
-o = parser.parse_args()
+def main(args=None):
 
-DATA = Path(f"/home/perrette/.local/share/vosk/language-models")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--language", nargs="+", choices=list(language_config_default),
+                        default=["en"])
+    o = parser.parse_args(args)
 
-language_config = {lang: language_config_default[lang] for lang in o.language}
+    DATA = Path(f"/home/perrette/.local/share/vosk/language-models")
 
-# Set up the microphone for recording
-micro = Microphone()
+    language_config = {lang: language_config_default[lang] for lang in o.language}
 
-# Chargez le modèle Vosk
-recognizers = {}
-for lang in o.language:
-    model_path = str(DATA/language_config[lang]["model"])
-    if not os.path.exists(model_path):
-        print(f"Please download the model for {lang} from https://alphacephei.com/vosk/models and unpack as {model_path}")
-        raise Exception(f"Vosk model not found for {lang}!")
+    # Set up the microphone for recording
+    micro = Microphone()
 
-    model = vosk.Model(model_path)
-    recognizers[lang] = vosk.KaldiRecognizer(model, micro.samplerate)
+    # Chargez le modèle Vosk
+    recognizers = {}
+    for lang in o.language:
+        model_path = str(DATA/language_config[lang]["model"])
+        if not os.path.exists(model_path):
+            print(f"Please download the model for {lang} from https://alphacephei.com/vosk/models and unpack as {model_path}")
+            raise Exception(f"Vosk model not found for {lang}!")
 
-# Commencer l'enregistrement
-def start_recording(language="fr"):
+        model = vosk.Model(model_path)
+        recognizers[lang] = vosk.KaldiRecognizer(model, micro.samplerate)
 
-    with micro.open_stream():
-        if language not in language_config:
-            raise ValueError(language)
-        meta = language_config[language]
+    # Commencer l'enregistrement
+    def start_recording(language="fr"):
 
-        print(meta["start message"])
-        rec = recognizers[language]
+        with micro.open_stream():
+            if language not in language_config:
+                raise ValueError(language)
+            meta = language_config[language]
 
-        try:
-            while True:
-                # Traiter les morceaux audio
-                while not micro.q.empty():
-                    data = micro.q.get()
-                    # print("Donnée audio reçue...")
-                    if rec.AcceptWaveform(data):
-                        result = rec.Result()
-                        result_dict = json.loads(result)
-                        # print(f"Transcription: {result_dict['text']}")
-                        clear_line()
-                        if len(result_dict['text']):
-                            print(result_dict['text'])
+            print(meta["start message"])
+            rec = recognizers[language]
 
-                    else:
-                        # Afficher le résultat brut pour comprendre pourquoi il est rejeté
-                        # print("Résultat brut non accepté par Vosk:")
-                        partial_result = rec.PartialResult()
-                        partial_result_dict = json.loads(partial_result)
-                        print_partial(partial_result_dict['partial'])
-                        continue
+            try:
+                while True:
+                    while not micro.q.empty():
+                        data = micro.q.get()
+                        if rec.AcceptWaveform(data):
+                            result = rec.Result()
+                            result_dict = json.loads(result)
+                            clear_line()
+                            if len(result_dict['text']):
+                                print(result_dict['text'])
 
-        except KeyboardInterrupt:
-            pass
+                        else:
+                            partial_result = rec.PartialResult()
+                            partial_result_dict = json.loads(partial_result)
+                            print_partial(partial_result_dict['partial'])
+                            continue
 
-        print(meta["stop message"])
+            except KeyboardInterrupt:
+                pass
 
-while True:
+            print(meta["stop message"])
+
     while True:
-        print("""Press a key to start recording:""")
-        for i, (lang, meta) in enumerate(language_config.items()):
-            print(f"({i+1}) {lang}: {meta['language']}")
-        res = input()
-        if res.lower() in ("q", "quit"):
-            exit(0)
-        candidates = {str(i+1): lang for i, lang in enumerate(language_config)}
-        candidates.update({lang.lower(): lang for lang in language_config})
-        if res == "":
-            res = "1"
-        if res not in candidates:
-            print("Invalid input.")
-            continue
-        language = candidates[res]
-        break
+        while True:
+            print("""Press a key to start recording:""")
+            for i, (lang, meta) in enumerate(language_config.items()):
+                print(f"({i+1}) {lang}: {meta['language']}")
+            res = input()
+            if res.lower() in ("q", "quit"):
+                exit(0)
+            candidates = {str(i+1): lang for i, lang in enumerate(language_config)}
+            candidates.update({lang.lower(): lang for lang in language_config})
+            if res == "":
+                res = "1"
+            if res not in candidates:
+                print("Invalid input.")
+                continue
+            language = candidates[res]
+            break
 
-    start_recording(language)
-    micro.q.queue.clear()
+        start_recording(language)
+        micro.q.queue.clear()
+
+
+if __name__ == "__main__":
+    main()
