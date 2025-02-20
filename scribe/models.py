82fc964adaaa71bdae9d54fbb8c26523bbaa48d2
perrette <mahe.perrette@pik-potsdam.de>
2025-02-21 00:45:12 +0100

    add openaiapi backend

--- scribe/models.py
+++ scribe/models.py
@@ -224,3 +224,33 @@ class WhisperTranscriber(AbstractTranscriber):
         result = self.transcribe_audio(self.audio_buffer)
         self.audio_buffer = b''
         return result
+
+
+class OpenaiAPITranscriber(WhisperTranscriber):
+    backend = "openaiapi"
+
+    def __init__(self, model_name="whisper-1", language=None, model_kwargs={}, model=None, api_key=None, **kwargs):
+        if model is None:
+            import openai
+            model = openai.OpenAI(
+                api_key=api_key or openai.api_key,
+                # 20 seconds (default is 10 minutes)
+                timeout=20.0,
+            )
+        AbstractTranscriber.__init__(self, model, model_name, language, model_kwargs=model_kwargs, **kwargs)
+
+    def transcribe_audio(self, audio_bytes):
+        self.log("\nTranscribing")
+        import io
+        import soundfile as sf
+        audio_data = np.frombuffer(audio_bytes, dtype=np.int16).flatten().astype(np.float32) / 32768.0
+        # Write the audio data to an in-memory file in WAV format
+        buffer = io.BytesIO()
+        sf.write(buffer, audio_data, self.samplerate, format='WAV')
+        buffer.seek(0)
+        buffer.name = "audio.wav"  # Set a filename with a valid extension
+        transcription = self.model.audio.transcriptions.create(
+            model=self.model_name,
+            file=buffer,
+        )
+        return {"text": transcription.text}
